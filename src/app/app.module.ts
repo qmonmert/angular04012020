443b31ad270e03418e45a0011743e03bf787c717
qmonmert <quentin.monmert@gmail.com>
2020-01-04 19:47:53 +0100

    Add angular material (ng add @angular/material)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialPageComponent } from './material-page/material-page.component';
 import { ClassicPageComponent } from './classic-page/classic-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ClassicPageComponent } from './classic-page/classic-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
